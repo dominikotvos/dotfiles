3ab70ed57ac9753cd04d7c192fc7f6d7ac61050d
lolsleuth <otvos85@gmail.com>
2025-04-03 12:02:11 +0200

    misc: indent blankline only in python

--- .config/nvim/lua/theprimeagen/lazy/indent-blankline.lua
+++ .config/nvim/lua/theprimeagen/lazy/indent-blankline.lua
@@ -2,6 +2,7 @@ return {
     "lukas-reineke/indent-blankline.nvim",
     main = "ibl",
     opts = {},
+    ft = { "python" },
     config = function()
         local highlight = {
             "CursorColumn",
