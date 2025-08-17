4f0744871d64d60d1fb6120d5e9bb30702f29e08
Dominik Ötvös <otvos85@gmail.com>
2025-08-17 19:30:28 +0200

    feat: added unreal engine config

--- .config/nvim/lua/theprimeagen/lazy/conform.lua
+++ .config/nvim/lua/theprimeagen/lazy/conform.lua
@@ -1,35 +1,38 @@
 return {
-	"stevearc/conform.nvim",
-	event = { "BufWritePre" },
-	config = function()
-		local conform = require("conform")
+    "stevearc/conform.nvim",
+    event = { "BufWritePre" },
+    config = function()
+        local conform = require("conform")
 
-		conform.setup({
-			formatters_by_ft = {
-				-- Python
-				python = { "black", "isort" },
+        conform.setup({
+            formatters_by_ft = {
+                -- Python
+                python = { "black", "isort" },
 
-				-- Go
-				go = { "gofmt" },
+                -- Go
+                go = { "gofmt" },
 
-				-- Add more formatters as needed
-				lua = { "stylua" },
-				javascript = { "prettier" },
-				typescript = { "prettier" },
+                -- Add more formatters as needed
+                lua = { "stylua" },
+                javascript = { "prettier" },
+                typescript = { "prettier" },
                 javascriptreact = { "prettier" },
                 typescriptreact = { "prettier" },
                 json = { "prettier" },
-			},
+                cpp = { "clang_format" },
+                h = { "clang_format" },
+                c = { "clang_format" },
+            },
 
-			format_on_save = false,
-		})
+            format_on_save = false,
+        })
 
-		-- Set up a key mapping to manually format
-		vim.keymap.set("n", "<leader>f", function()
-			conform.format({
-				async = true,
-				lsp_fallback = true,
-			})
-		end, { desc = "Format file" })
-	end,
+        -- Set up a key mapping to manually format
+        vim.keymap.set("n", "<leader>f", function()
+            conform.format({
+                async = true,
+                lsp_fallback = true,
+            })
+        end, { desc = "Format file" })
+    end,
 }
