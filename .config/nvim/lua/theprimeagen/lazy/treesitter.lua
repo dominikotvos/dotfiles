f3f129994e65863b6492abf1a158f76395aa26dc
Dominik Ötvös <dominik.otvos@falseheavengames.com>
2026-05-04 13:49:46 +0200

    changed treesitter to main and other random changes

--- .config/nvim/lua/theprimeagen/lazy/treesitter.lua
+++ .config/nvim/lua/theprimeagen/lazy/treesitter.lua
@@ -1,19 +1,30 @@
 return {
     "nvim-treesitter/nvim-treesitter",
-    branch = "master",
+    branch = "main",
+    lazy = false,
     build = ":TSUpdate",
     config = function()
-        require("nvim-treesitter.configs").setup({
-            ensure_installed = {
-                "vimdoc", "javascript", "typescript", "c", "lua", "rust",
-                "jsdoc", "bash", "gdscript",
-            },
-            auto_install = true,
-            highlight = {
-                enable = true,
-            },
+        local parsers = {
+            "vimdoc", "javascript", "typescript", "c", "lua", "rust",
+            "jsdoc", "bash", "gdscript", "templ",
+        }
+
+        require("nvim-treesitter").setup({
+            install_dir = vim.fn.stdpath("data") .. "/site",
         })
 
         vim.treesitter.language.register("templ", "templ")
+
+        require("nvim-treesitter").install(parsers)
+
+        vim.api.nvim_create_autocmd("FileType", {
+            pattern = {
+                "help", "vimdoc", "javascript", "typescript", "c", "lua", "rust",
+                "bash", "gdscript", "templ",
+            },
+            callback = function(args)
+                pcall(vim.treesitter.start, args.buf)
+            end,
+        })
     end
 }
